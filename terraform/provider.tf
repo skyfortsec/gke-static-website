319628db5645dce5eac3e63d117d0200889a9ca3
skyfortsec <itjoeldazz@gmail.com>
2025-07-05 12:15:09 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,12 +1,18 @@
+# Google Cloud provider configuration
 provider "google" {
   project = var.project_id
-  region  = var.zone
+  region  = var.region
+  zone    = var.zone
 }
 
+# Kubernetes provider to interact with the GKE cluster
 provider "kubernetes" {
   host                   = google_container_cluster.primary.endpoint
   token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+  cluster_ca_certificate = base64decode(
+    google_container_cluster.primary.master_auth[0].cluster_ca_certificate
+  )
 }
 
+# Get authenticated account config for access token
 data "google_client_config" "default" {}
